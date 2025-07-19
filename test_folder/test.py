4d6f72285afbec66563a989585b791d6df7d9c9f
aditiverma <aditi0603verma@gmail.com>
2025-07-19 11:39:18 -0500

    first python file

---
+++ test_folder/test.py
@@ -0,0 +1,12 @@
+
+# Example arrays
+array1 = [1, 2, 3]
+array2 = [4, 5, 6]
+
+# Add the arrays element-wise without using numpy
+result = [a + b for a, b in zip(array1, array2)]
+
+# Add the arrays element-wise (already done above)
+# result = array1 + array2
+
+print("Sum of arrayssss:", result)
