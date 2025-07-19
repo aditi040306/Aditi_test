1e0347e46938b75e784caf379c8ed6b5db8ba36b
aditi040306 <aditi0603verma@gmail.com>
2025-07-19 11:43:52 -0500

    Update1 test.py

--- test_folder/test.py
+++ test_folder/test.py
@@ -8,6 +8,6 @@ result = [a + b for a, b in zip(array1, array2)]
 
 # Add the arrays element-wise (already done above)
 # result = array1 + array2
-# adding two arrays
+# adding two arrays changes
 
 print("Sum of arrays:", result)
